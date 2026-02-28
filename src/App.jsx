e86dead48d82732aa74095883b47f11b07e1c167
Cristian Gutierrez <cristiangutierrez@MacBook-Pro-de-Cristian.local>
2026-02-28 22:45:47 +0100

    Fix /dining route redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
-
 import React from 'react';
 import { LanguageProvider } from './context/LanguageContext';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './components/Layout';
 import Dashboard from './components/Dashboard';
 import Itinerary from './components/Itinerary';
@@ -14,8 +13,10 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Dining />} />
+            <Route path="dining" element={<Navigate to="/" replace />} />
             <Route path="itinerary" element={<Itinerary />} />
             <Route path="dashboard" element={<Dashboard />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
       </BrowserRouter>
